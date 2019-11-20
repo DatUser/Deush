41bcb1f74f50ca07e67ee488f8bca1ae4eb5f493
Diogo SIMAO MARQUES <diogo.simao-marques@epita.fr>
2019-11-20 19:48:10 +0100

    [ADD][lexer.c][lexer.h] Pipelines
        lexer.c : Add of the functions related to the pipelines
        lexer.h : The functions related to the pipelines are now defined in lexer.h

--- src/lexer/header/lexer.h
+++ src/lexer/header/lexer.h
@@ -11,4 +11,10 @@ int is_elif(char *input, size_t *index, size_t len);
 int is_fi(char *input, size_t *index, size_t len);
 int is_command(char *input, size_t *index, size_t len);
 
+int add_bang(char *input, size_t *index, size_t len);
+int add_pipe(char *p, size_t *index);
+int pipelines(char *input, size_t *index, size_t len);
+
+void add_newline(void);
+
 #endif
