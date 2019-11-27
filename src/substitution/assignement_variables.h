a2bff01a22edcade0ec2794f9180804c385947dd
Philemon Duval <philemon.duval@epita.fr>
2019-11-27 14:44:33 +0100

    [ADD][VARIABLES] add variables struct and basic function

---
+++ src/substitution/assignement_variables.h
@@ -0,0 +1,20 @@
+
+
+#ifndef ASSIGNEMENT_VARIABLES_H
+#define ASSIGNEMENT_VARIABLES_H
+#include "../include/global.h"
+struct variables
+{
+    char *name;
+    char *value;
+    struct variables *next;
+}
+
+extern struct variables *variables;
+void add_variable(char *name, char *value);
+int is_nbr(char *name);
+char *variable_value(char *name);
+void free_variables(struct variables *curr);
+int generate_random(void);
+char *shellopts(void);
+#endif
