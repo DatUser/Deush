c3604756c3a77792f3159f57f2eb20227b28b1fd
clement.leconte <clement.leconte@epita.fr>
2019-11-27 22:14:29 +0100

    simple quotes now quotes

--- src/quoting/quoting.c
+++ src/quoting/quoting.c
@@ -2,6 +2,8 @@
 #include "../include/global.h"
 #include "header/quoting.h"
 
+size_t unquote_squotes(char *input, size_t *index, size_t len);
+
 int SQUO = 0;
 
 static void shift(char *input, size_t index, size_t len)
@@ -11,18 +13,24 @@ static void shift(char *input, size_t index, size_t len)
         input[index] = input[index + 1];
         index++;
     }
+    input[len - 1] = '\0';
 }
 
 size_t unquote_backslashes(char *input, size_t *index, size_t len)
 {
-    while (*index < len)
+    while (*index < strlen(input))
     {
         if (input[*index] == '\\')
         {
-            shift(input, *index, len);
+            shift(input, *index, strlen(input));
             len--;
         }
+        else if (input[*index] == '\'')
+        {
+            unquote_squotes(input, index, strlen(input));
+        }
         *index += 1;
+
     }
     return len;
 }
@@ -56,7 +64,7 @@ size_t unquote_squotes(char *input, size_t *index, size_t len)
 
 int main(void)
 {
-    char to_shift[] = "'\\a\\\\\\b\\c\\d'\\\'e\f'g'\0";
+    char to_shift[] = "'\\a\\\\\\b\\c\\d'\\\'e\\f\'g\'";
     size_t index = 0;
     while (index < strlen(to_shift))
     {
@@ -74,4 +82,5 @@ int main(void)
             index++;
         }
     }
+    printf("%s\n", to_shift);
 }
