67a203511ded1b140e743a4d1be907155921a6dd
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2019-11-30 14:35:12 +0100

    [FIX][builtin_exec.c]
        Fix the history load when the interactive mode is launched.
        Fix the cd without arguments.

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -421,12 +421,36 @@ void parse2(void)
 
 void print_hist_list(void);
 
+
+void load_hist_list(void)
+{
+    FILE *file = fopen(path, "r");
+    if (file == NULL)
+    {
+        return;
+    }
+
+    size_t len = 0;
+    char *line = NULL;
+    ssize_t read = getline(&line, &len, file);
+    while (read != -1)
+    {
+        add_history(line);
+        read = getline(&line, &len, file);
+    }
+    free(line);
+    fclose(file);
+}
+
+
+
 /*!
 **  This function launches the interactive mode.
 */
 void interactive_mode(void)
 {
     signal(SIGINT, signal_callback_handler);
+    load_hist_list();
     char *line = get_next_line(PS1);
     char *tmp;
     char *s = NULL;
@@ -735,7 +759,7 @@ void destroy_hist(struct line *l)
 */
 int history(void)
 {
-    FILE *f = fopen(home, "r");
+    FILE *f = fopen(path, "r");
     if (f == NULL)
     {
         return 0;
@@ -906,7 +930,8 @@ int main(int argc, char *argv[])
     using_history();
     tmp_histo = init_histo_list();
     home = getenv("HOME");
-    path = strcat(home, file_name);
+    char *home_cpy = strdup(home);
+    path = strcat(home_cpy, file_name);
 
     environ = argc + argv + 1;
 
@@ -992,6 +1017,7 @@ int main(int argc, char *argv[])
     lexer = re_init_lexer(lexer);
     free(lexer);
     free_variables(variables);
+    free(home_cpy);
 
     return last_return_value;
 }
