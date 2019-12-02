c985b3b110c2a97a6f29e9a6919cf684dbd868a1
Philemon Duval <philemon.duval@epita.fr>
2019-12-02 20:19:55 +0100

    [fix][last return_value] fixed the delete after
    script

--- src/substitution/assignement_variables.c
+++ src/substitution/assignement_variables.c
@@ -333,7 +333,7 @@ void script_del_args(void)
     pop_variable("*");
     pop_variable("@");
     pop_variable("$");
-    pop_variable("?");
+    //pop_variable("?");
     pop_variable("#");
 }
 void init_variables(void)
