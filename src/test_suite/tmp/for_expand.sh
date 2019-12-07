fb360cc7006f75db1ace2a2af35f87528156ecfa
clement.leconte <clement.leconte@epita.fr>
2019-12-07 18:32:33 +0100

    [ADD] add some test and update test suite

---
+++ src/test_suite/tmp/for_expand.sh
@@ -0,0 +1,5 @@
+i='0 1 2'
+for j in $i
+    do
+        echo popo
+    done
---
+++ src/test_suite/tmp/no_matche.sh
@@ -0,0 +1,11 @@
+i=1
+
+case $i in
+    hello)
+        echo popo
+        ;;
+    0)
+        echo salut
+        ;;
+esac
+
