03e4d6de18e939a165158c9faa6775d56d9f00cc
clement.leconte <clement.leconte@epita.fr>
2019-12-07 18:32:33 +0100

    [ADD] add some test and update test suite

--- src/test_suite/testsuite.py
+++ src/test_suite/testsuite.py
@@ -73,7 +73,7 @@ def lunch(categorie,i, t = 0):
     return (passed,total)
 
 if __name__ == "__main__":
-    categories = ["command","input","buil_exec"]
+    categories = ["command","buil_exec", "case" , "for"]
     parser = ArgumentParser(description="42sh Testsuite")
     parser.add_argument('bin', metavar='BIN')
     parser.add_argument('--list',"-l",help="list all categories",action = "store_true")
